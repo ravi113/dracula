f605676c17d74fe76d4ec7c80465d1dbb5bf51e2
Quan Hoang <quanhm82@gmail>
2014-12-27 15:13:32 +0700

    Adding more logic to parse login

--- UnitySource/Assets/Scripts/ParseLoginScript.cs
+++ UnitySource/Assets/Scripts/ParseLoginScript.cs
@@ -1,15 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Parse;
+using UnityEngine.UI;
 
 public class ParseLoginScript : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
+	public Text usernameField;
+	public Text passwordField;
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	public void Login() {
+		string usernameVal = usernameField.text;
+		string pwdVal = passwordField.text;
+
+		if (!string.IsNullOrEmpty (usernameVal)) {
+		 	ParseUser.LogInAsync(usernameVal, pwdVal).ContinueWith(t => {
+				if(t.IsFaulted || t.IsCanceled) {
+					// Login fail
+					//Text status = GameObject.Find("statusText").GetComponent<Text>();
+					//status.text = "Fail to login";
+					Debug.LogWarning("Fail to login");
+				}
+				else {
+					// Login Success
+					usernameField.enabled = false;
+					passwordField.enabled = false;
+					usernameField.text = "welcome " + ParseUser.CurrentUser.Username;
+				}
+			});
+		}
 	}
 }
